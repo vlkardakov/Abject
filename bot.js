c3c8206feecaebf10c11ab74c7a425b24c06c4a2
vlkardakov <vlkardakov@gmail.com>
2025-04-13 03:37:55 +0300

    d

--- bot.js
+++ bot.js
@@ -845,8 +845,8 @@ bot.on('message', (jsonMsg, position) => {
                 })
 
                 const chestBlock = blocks
-                    .map(pos => bot.blockAt(pos))
-                    .find(block => block && block.position.y === 85 && block.position.z === 7)
+                    .map(pos => bot.blockAt(pos)) 
+                    .find(block => block && block.position.y === 86 && block.position.z === 8)
 
                 console.log(`Distnace to barrel: ${bot.entity.position.distanceTo(chestPos)}`);
                 if (!chestBlock) {
