f72d66d46842b2e8b62add23841b2c4b80b0ce9a
vlkardakov <vlkardakov@gmail.com>
2025-04-14 04:26:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -360,7 +360,7 @@ async function breakBlockManually(block) {
 async function connectToServer() {
     console.log('Пытаюсь зайти!');
     await new Promise(resolve => setTimeout(resolve, 500));
-    bot.chat('/server sleepcraft');
+    // bot.chat('/server sleepcraft');
 }
 async function sendFeedback(text) {
     for (const player of WATCHED_PLAYERS) {
