95743b3be017cdfd8f4e1959fab476930ffe2bc1
vlkardakov <vlkardakov@gmail.com>
2025-04-22 17:15:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -696,7 +696,7 @@ function processCommand(message, username, plainMessage) {
             return;
         case 'load-music':
             console.log('Начали')
-            if (!args.length < 1 ) {return;}
+            if (args.length < 1 ) {return;}
             console.log('Начали')
             const songName = args[0];
             const fileName = args[1] || songName.toLowerCase().replace(/ /g, '_') + '.mp3';
