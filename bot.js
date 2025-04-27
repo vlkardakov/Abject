13cead0d60ed3251bd87561c04c004727f1fc502
vlkardakov <vlkardakov@gmail.com>
2025-04-27 17:31:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1472,8 +1472,11 @@ function processCommand(message, username, plainMessage) {
         case "health":
             bot.chat(`/msg ${username} ${bot.health}`);
             break
+        case "cords":
+            bot.chat(`/r ${parseInt(bot.entity.position.x)} ${parseInt(bot.entity.position.y)} ${parseInt(bot.entity.position.z)}`)            break
         case "quit":
             bot.chat(`/msg ${username} Самоуничтожение...`);
+            bot.quit()
             break
         case "chosecolor":
             const targetBlock = bot.findBlock({
