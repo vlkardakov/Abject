07927d091e0882bcc640f46351228fed23ebea7a
vlkardakov <vlkardakov@gmail.com>
2025-04-29 18:27:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -622,7 +622,6 @@ async function depositItems() {
     justCheckedBarrel = true
     chestPos = vec3(2, 82, -33);
     await bot.pathfinder.goto(new goals.GoalNear(chestPos.x, chestPos.y, chestPos.z, 1));
-    await unequipArmorAndMainHand()
     const blocks = bot.findBlocks({
         matching: block => block.name.includes('barrel'),
         maxDistance: 10,
@@ -640,7 +639,7 @@ async function depositItems() {
     //         return;
     //     }
     //
-    //     await unequipArmorAndMainHand()
+    await unequipArmorAndMainHand()
     //
     //     // const blockToLookAt_rich = bot.findBlock({
     //     //     matching: block => {
