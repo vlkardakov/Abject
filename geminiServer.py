7e88aa4ae8214d08747697045b5bc23b72fee8e0
vlkardakov <vlkardakov@gmail.com>
2025-05-06 16:31:44 +0300

    Update geminiServer.py

--- geminiServer.py
+++ geminiServer.py
@@ -130,4 +130,4 @@ def ask_api():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=4345, debug=True)
