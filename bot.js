3fb79a0b601a1a6b51b4cd921a3d62f661e954e6
vlkardakov <vlkardakov@gmail.com>
2025-05-08 14:46:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -926,8 +926,8 @@ async function craftSet(count = 1) {
     const stickBarrel = new vec3({x:2, y:85, z:6})
     const chickenBarrel = new vec3({x:2, y:85, z:5})
 
-    await takeItem(ironBarrel, 'iron_ingot', count=count*26)
-    await takeItem(stickBarrel, 'stick', count=count*2)
+    await takeItem(ironBarrel, 'iron_ingot', count=26)
+    await takeItem(stickBarrel, 'stick', count=2)
     await takeItem(chickenBarrel, 'cooked_chicken', count=count*9)
 
     await craftItem('iron_helmet', count = 1)
