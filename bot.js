bdfd299eb7ed9c356aad2a52dc1d71ad42f08a13
vlkardakov <vlkardakov@gmail.com>
2025-05-15 13:15:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -882,7 +882,7 @@ async function takeItem(blockPos, itemName, count = 1) {
     // try {
         await bot.lookAt(blockPos, true)
         const chest = await bot.openContainer(block)
-        const items = chest.containerItems().filter(i => i.name === itemName)
+        const items = chest.containerItems().filter(i => i && i.name === itemName)
 
         if (!items) {
             chest.close()
