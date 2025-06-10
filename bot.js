509ae9b7379cdc6e7c7c15d7ee684479a988d2c2
vlkardakov <vlkardakov@gmail.com>
2025-06-10 19:25:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1425,7 +1425,7 @@ function processCommand(message, username, plainMessage) {
                         bot.chat(`/msg ${WATCHED_PLAYERS[0]} Иду!`)
                         bot.pathfinder.setMovements(defaultMove);
                         bot.pathfinder.setGoal(null)
-                        bot.pathfinder.setGoal(new GoalFollow(my_item, 0));
+                        bot.pathfinder.setGoal(new GoalNear(my_item.position, 0.1   ));
                     } else if (targetEnemy ) {
                         if (targetEnemy !== oldTargetEnemy) {
                         oldTargetEnemy = targetEnemy;// console.log('Нормальный предмет detetcted!')
