ab367df398530d380b1474d1bed5dc6a1fc8b403
vlkardakov <vlkardakov@gmail.com>
2025-06-12 15:35:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1111,7 +1111,7 @@ bot.on('entityHurt', async (entity) => {
     }
 })
 bot.on('physicsTick', () => {
-    `if (bot.entity.velocity.y < -0.2 && ANTIFALL) bot.entity.velocity.y += 0.4
+    if (bot.entity.velocity.y < -0.2 && ANTIFALL) bot.entity.velocity.y += 0.4
 }) 
 function processCommand(message, username, plainMessage) {
     const parts = message.trim().toLowerCase().split(" ");
