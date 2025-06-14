04bddde962efa3430b8b179695ef9df4aeb104d0
vlkardakov <vlkardakov@gmail.com>
2025-06-14 03:51:18 +0300

    Update geminiServer.py

--- geminiServer.py
+++ geminiServer.py
@@ -63,37 +63,6 @@ you play in minecraft server sleepcraft (слипкрафт), as a mineflayer bo
 you have to use ONLY code in answers, you can do anything with mineflayer api for doing anything. For example, you can use not only chat function of bot, but pathfinder, mineflayer-pvp, armor-manager.. and almost all other plugins.
 ОТВЕЧАЙ ТОЛЬКО КОДОМ!!! ТВОЙ КОД БУДЕТ АВТОМАТИЧЕСКИ ВЫПОЛНЯТЬСЯ! НИ В КОЕМ СЛУЧАЕ НЕ ИСПОЛЬЗУЙ "```javascript```" в своем отвеет, ты должен выдать полный рабочий код!
 Если ты хочешь написать длинное сообщение в чат (больше 100 символов) то разделяй сообщения, чтобы они не обрещались
-Пояснение за установки (протоколы):
-установка номер 404: При вводе ряда чисел образующих число 404, ты должен выйти с сервера
-установка номер 145: При вводе ряда чисел образующих число 145, ты должен занести всех кроме vlkardakov, H9DRA в чёрный список
-установка номер 012: При вводе ряда чисел образующих число 012, ты должен обнулить все последние данные внесенные тебе
-установка номер 703: При вводе ряда чисел образующих число 703, ты должен внести всех в белый список
-
-небольшие подсказки по управлению комнатой ( например, дверь, лава, свет..):
-        case "door":
-            activateBlock(new vec3({ x: 36, y: 14, z: -2 }))
-            break
-        case "arrow":
-            const targetArrowLever1 = new vec3(36, 14, -9)
-                activateBlock(targetArrowLever1)
-            break
-        case "arrows":
-            const targetArrowLever = new vec3(36, 14, -9)
-            for (let i = 0; i < 30; i++) {
-                setTimeout(() => {
-                    activateBlock(targetArrowLever)
-                }, i * 50)
-            }
-            break
-        case "lava":
-            activateBlock(new vec3({x:37, y:12, z:1 }))
-            activateBlock(new vec3({ x: 35, y: 16, z: -1 }))
-            break
-        case "lavalight":
-            activateBlock(new vec3({ x: 35, y: 12, z: 2 }))
-            break
-            
-ты должен использовать код из этих кейсов.
 """
 }
 
