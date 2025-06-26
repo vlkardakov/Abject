e33ea66b8058bb7a5da0da78f7b489da974a5bcf
vlkardakov <vlkardakov@gmail.com>
2025-06-26 17:24:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1019,7 +1019,7 @@ function getY(height, speed = 20, tasked=true) {
                 bot.entity.velocity.y = speed;
                 console.log('продолжаю')
             } else {
-                console.log(`прекращаю ,task = ${task}`)
+                console.log(`прекращаю ,task = ${task}, `)
                 bot.removeListener('physicsTick', onTick);
                 bot.entity.velocity.y = 0
                 if (tasked) task = null
@@ -2757,7 +2757,16 @@ bot.once('login', () => {
 });
 
 bot.on("plasmovoice_audio_end", () => {
-    SOUND = null
+    try {
+        SOUND = null
+    } catch (err) {
+        if (err.message.includes("Invalid typed array length")) {
+            console.warn("Ошибка в аудиоданных:", err.message)
+            return;
+        }
+        // throw err;
+    }
+
 })
 
 bot.on('entitySpawn', (entity) => {
