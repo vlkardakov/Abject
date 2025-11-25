fa8ac7ac6f31d7497755e04f1af9908606d47f6b
vlkardakov <vlkardakov@gmail.com>
2025-11-25 21:20:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,8 @@ console.log('----------------')
 //ЭТО КОММЕНТАРИЙ ДОЛЖЕН ПОЯВИТЬСЯ!
 const bot = mineflayer.createBot({
     // host: '212.80.7.178', //or
-    host: 'sleepcraft.ru', //or
+    // host: 'sleepcraft.ru', //or
+    host: 'serenity.su', //or
     // host: '87.120.187.6', //or
     port: 25565,
     username: BOT_USERNAME,
